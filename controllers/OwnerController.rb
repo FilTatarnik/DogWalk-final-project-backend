575c10342ffcba1cd2bb5c22832f67370d76581a
Filip Tatarnik <ftatarnik@gmail.com>
2018-11-29 11:33:00 -0600

    Owner can now register, login, logout

--- controllers/OwnerController.rb
+++ controllers/OwnerController.rb
@@ -1,4 +1,72 @@
 class OwnerController < ApplicationController
 
+	before do
+		puts ""
+		puts "here's the session right now in the filter"
+		pp session
+	end
+#-----------------REGISTER------------------#
+		post '/register' do
+			#recieve JSON requests
+			payload_body = request.body.read
+			payload = JSON.parse(payload_body).symbolize_keys
+			email = payload[:email]
+			owner_exists = Owner.find_by email: payload[:email]
 
+			if owner_exists
+				{
+					status: 421,
+					message: "username taken"
+				}.to_json
+			else
+				owner = Owner.new
+				owner.email = payload[:email]
+				owner.password = payload[:password]
+				
+				owner.save
+				session[:logged_in] = true
+				session[:email] = owner.email
+				{
+					status: 200,
+					message: "Successfully registered owner #{owner.email}"
+				}.to_json
+			end
+		end
+#-----------------LOGIN------------------#
+		post '/login' do
+			payload_body = request.body.read
+			payload = JSON.parse(payload_body).symbolize_keys
+
+			owner = Owner.find_by email: payload[:email]
+			pw = payload[:password]
+
+			if owner and owner.authenticate(pw)
+				session[:logged_in] = true
+				session[:email] = owner.email
+		
+				puts ""
+				puts "here's the session right now after we logged in"
+				pp session
+
+				{
+					status: 200,
+					message: "Successfully logged in"
+				}.to_json
+			else
+				{
+					status: 403,
+					message: "Invalid email or password"
+				}.to_json
+			end
+		end
+#-----------------LOGOUT------------------#
+		get '/logout' do
+			# binding.pry
+			email = session[:email]
+			session.destroy
+			{
+				status: 200,
+				message: "Logged out owner"
+			}.to_json
+		end
 end
